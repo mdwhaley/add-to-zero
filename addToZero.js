24ea4d17523b38b64370877f12000312a69bacd9
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-24 09:33:00 -0500

    format

--- addToZero.js
+++ addToZero.js
@@ -2,12 +2,12 @@
 let array = [28, 43, -12, 30, 4, 0, 12];
 
 // Write your solution below:
-for (i = 0; i < (array.length - 1); i++) {
-    for (a = (i + 1); a < array.length; a++) {
-        if (array[i] + array[a] === 0) {
-            console.log('true');
-        } else {
-            console.log('false')
-        }
+for (i = 0; i < array.length - 1; i++) {
+  for (a = i + 1; a < array.length; a++) {
+    if (array[i] + array[a] === 0) {
+      console.log("true");
+    } else {
+      console.log("false");
     }
+  }
 }
